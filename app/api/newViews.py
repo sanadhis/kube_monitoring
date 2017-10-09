7f328919fbe92d2a3296bde0a50bbd646dfe373e
sanadhis <sanadhis@gmail.com>
2017-10-09 17:14:57 +0200

    Init Web and Templating Jinja2

--- app/api/newViews.py
+++ app/api/newViews.py
@@ -4,10 +4,9 @@ from rest_framework.renderers import JSONRenderer
 from influxdb_metrics.utils import query
 import json
 
-queryDB       = 'SELECT * from h2o_feet LIMIT 3'
-h2o_data = query(queryDB)
-
 def index(request):
+    queryDB  = 'SELECT * from h2o_feet LIMIT 3'
+    h2o_data = query(queryDB)
     result = [x for x in h2o_data.get_points()]
     return JsonResponse(result,safe=False)
     #return HttpResponse(result)
--- app/web/views.py
+++ app/web/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
+from influxdb_metrics.utils import query
 
 def index(request):
+    queryDB  = 'SELECT * from h2o_feet LIMIT 3'
+    h2o_data = query(queryDB)
     template = loader.get_template('adminlte/base.html')
-    context = {}
+    result = [x for x in h2o_data.get_points()]    
+    context = {
+        'result': result,
+    }
     return HttpResponse(template.render(context,request))

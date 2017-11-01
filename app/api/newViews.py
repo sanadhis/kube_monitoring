e49ec449bc5a6a456730d32da5fe995a1a142ef1
sanadhis <sanadhis@gmail.com>
2017-11-01 11:35:39 +0100

    Update Views

--- app/api/newViews.py
+++ app/api/newViews.py
@@ -1,12 +1,26 @@
 from influxdb import InfluxDBClient
 from django.http import HttpResponse, JsonResponse
 from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions
 from influxdb_metrics.utils import query
+from requests.exceptions import ConnectionError
 import json
 
+@api_view(['GET', 'POST'])
+@permission_classes((permissions.AllowAny,))
 def index(request):
-    queryDB  = 'SELECT * from h2o_feet LIMIT 3'
-    h2o_data = query(queryDB)
-    result = [x for x in h2o_data.get_points()]
-    return JsonResponse(result,safe=False)
-    #return HttpResponse(result)
+    if request.method == 'GET':
+        try:
+            queryDB  = 'SELECT * from h2o_feet LIMIT 3'
+            h2o_data = query(queryDB)
+            result = [x for x in h2o_data.get_points()]
+            return JsonResponse(result,safe=False)
+        except ConnectionError as e:
+            content = {'please move along': 'nothing to see here'}
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
+    elif request.method == 'POST':
+        body = request.body
+        return HttpResponse(body)
--- app/web/views.py
+++ app/web/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 from influxdb_metrics.utils import query
 
 def index(request):
-    queryDB  = "SELECT * FROM \"cpu/usage_rate\" WHERE namespace_name = 'default' ORDER BY time DESC LIMIT 100 "
+    queryDB  = "SELECT * FROM \"cpu/usage_rate\" WHERE (namespace_name = 'default' or namespace_name='vlsc' or namespace_name='dcsl') ORDER BY time DESC LIMIT 100 "
     h2o_data = query(queryDB)
     template = loader.get_template('adminlte/base.html')
     result = [x for x in h2o_data.get_points()]    

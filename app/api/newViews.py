8f807475f186ac3d8f928e18b14146c1061d32a0
sanadhis <sanadhis@gmail.com>
2017-11-21 14:45:25 +0100

    Update Api REST

--- app/api/newViews.py
+++ app/api/newViews.py
@@ -4,19 +4,27 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
 from influxdb_metrics.utils import query
 from requests.exceptions import ConnectionError
+from influxdb.exceptions import InfluxDBClientError
 import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 @api_view(['GET', 'POST'])
 @permission_classes((permissions.AllowAny,))
 def index(request):
     if request.method == 'GET':
         try:
-            queryDB  = 'SELECT * from h2o_feet LIMIT 3'
-            h2o_data = query(queryDB)
-            result = [x for x in h2o_data.get_points()]
+            queryDB  = 'SELECT * from "gpu/usage" LIMIT 10'
+            gpu_data = query(queryDB)
+            result = [x for x in gpu_data.get_points()]
             return JsonResponse(result,safe=False)
         except ConnectionError as e:
             message = {"code":500,"message":"Can't connect to influxdb"}
+            return JsonResponse(message,status=500)
+        except InfluxDBClientError as e:
+            message = {"code":500,"message":"Wrong configuration to influxdb"}
+            logger.error(e)
             return JsonResponse(message,status=500)
         
     elif request.method == 'POST':

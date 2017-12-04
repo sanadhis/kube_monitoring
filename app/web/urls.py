7e54e09e557857e0f55d5b82d800f4208f7bfd67
sanadhis <sanadhis@gmail.com>
2017-12-04 15:23:12 +0100

    Fix URL

--- app/web/urls.py
+++ app/web/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 
-from . import views
+from . import views, authentication
 
 urlpatterns = [
-    url(r'^', views.index, name='index'),
+    url(r'^login/', views.render_login, name='login'),
+    url(r'^stats/', views.index, name='index'),
+    url(r'^authentication/signin', authentication.signin, name='signin'),
+    url(r'^authentication/signout', authentication.signout, name='signout'),    
+    url(r'^$', views.index, name='index'),    
 ]

fcdc78c1b4b8a7b99f940de531357c64e220188a
Unknown <sanadhis@gmail.com>
2017-12-04 13:20:25 +0100

    Add Static files Dir

--- app/kube_monitoring/settings.py
+++ app/kube_monitoring/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/web/static/'
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'web/static/'),
+)
 
 ### REST FRAMEwORK SETTINGS ###
 REST_FRAMEWORK = {
